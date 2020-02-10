68d571cffe19facf6620a5cc6fe80bb30f50ade0
ye2k <529129461@qq.com>
2020-02-10 11:32:24 +0800

    项目样式处理

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div class="demo">
       app
+      <i class="iconfont iconwode"></i>
     </div>
-    <router-view/>
   </div>
 </template>
+
+<style lang="scss">
+.demo {
+    width:200px;
+    height:200px;
+    background-color: pink;
+    font-size: 24px;
+    i{
+      font-size: 24px;
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'lib-flexible'
+
+import './styles/base.css'
+import './styles/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
